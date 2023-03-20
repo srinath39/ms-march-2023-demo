84162085126658beb7f3a4c168a9bfb1ae9f5014
srinath39 <96327215+srinath39@users.noreply.github.com>
2023-03-20 17:46:49 +0530

    version

---
+++ src/main/java/com/employee/source/EmployeeDetails.java
@@ -0,0 +1,58 @@
+package com.employee.source;
+
+public class EmployeeDetails {
+	private int id;
+	private String name;
+	private String gender;
+	private String phno;
+	private String email;
+	private String designation;
+	private double salary;
+	@Override
+	public String toString() {
+		return "EmployeeDetails [id=" + id + ", name=" + name + ", gender=" + gender + ", phno=" + phno + ", email="
+				+ email + ", designation=" + designation + ", salary=" + salary + "]";
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getPhno() {
+		return phno;
+	}
+	public void setPhno(String phno) {
+		this.phno = phno;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+}
